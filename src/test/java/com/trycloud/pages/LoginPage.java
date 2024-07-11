0cfd77ceb17fafdea4488e13f52883e10ab11689
olgashyyan.uk@gmail.com <olgashyyan.uk@gmail.com>
2024-07-11 16:58:28 -0400

    Deck feature and login feature

---
+++ src/test/java/com/trycloud/pages/LoginPage.java
@@ -0,0 +1,30 @@
+package com.trycloud.pages;
+
+import com.trycloud.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    public LoginPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(id = "user")
+    public WebElement usernameInput;
+
+    @FindBy(id = "password")
+    public WebElement passwordInput;
+
+    @FindBy(id = "submit-form")
+    public WebElement submitButton;
+
+
+    public void login(String username, String password) {
+        usernameInput.sendKeys(username);
+        passwordInput.sendKeys(password);
+        submitButton.click();
+
+    }
+}
---
+++ src/test/resources/features/login.feature
@@ -0,0 +1,6 @@
+@wip
+Feature: Login functionality
+
+  Scenario: As a user, I should be able to log in
+    Given user is on the login page
+    Given user logged in with username as "user1" and password as "Userpass123"
